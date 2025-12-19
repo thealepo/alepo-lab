76b09652a0a56bbb4292fd120441bcdaae93a049
thealepo <sanchezc.alexe@gmail.com>
2025-12-19 11:02:28 -0500

    debugged mlp class

--- karpathy-zero-to-hero/micrograd/mlp.py
+++ karpathy-zero-to-hero/micrograd/mlp.py
@@ -21,9 +21,9 @@ class Layer:
         return out
 
 class MLP:
-    def __init__(self , nin , nouts):
+    def __init__(self , nin: int , nouts: list):
         size = [nin] + nouts
-        self.layers = [Layer(size[i], size[i+1]) for i in range(nouts)]
+        self.layers = [Layer(size[i], size[i+1]) for i in range(len(nouts))]
 
     def __call__(self , x):
         for layer in self.layers:
